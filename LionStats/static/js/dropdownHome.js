02066eff20bc5943f8cbea0fd8f87cc2cd6a39fe
ConnorWMcClellan <mcclellanconnor7@gmail.com>
2022-05-03 00:15:26 -0500

    commit 5/3

--- LionStats/static/js/dropdownHome.js
+++ LionStats/static/js/dropdownHome.js
@@ -32,7 +32,55 @@ for (var i = 0; i < teamData.data.length; i++)
 window.onload = function()
 {
     let btn = document.getElementById("dropdown");
-
     dropdown();
+
+    btn.onchange = async function(){
+
+//    await new Promise(r => setTimeout(r, 5000));
+    $.get("http://localhost:8000/api/home",function(data) {
+
+    var endpoint = '/api/home'
+    var teamData = []
+    var labels = []
+    $.ajax({
+    method: "GET",
+    url: endpoint,
+    success: function(data){
+        labels = data.labels
+        teamData = data.default
+    const ctx = document.getElementById('stat1').getContext('2d');
+    const stat1 = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: data.labels,
+        datasets: [{
+            data: teamData,
+            backgroundColor: [
+                'rgba(253, 181, 37, 0.7)',
+                'rgba(253, 181, 37, 0.4)',
+                'rgba(253, 181, 37, 0.7)',
+                'rgba(253, 181, 37, 0.4)',
+                'rgba(253, 181, 37, 0.7)',
+                'rgba(253, 181, 37, 0.4)',
+                'rgba(253, 181, 37, 0.7)',
+                'rgba(253, 181, 37, 0.4)',
+                'rgba(253, 181, 37, 0.7)',
+            ],
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
+}
+    });
+});
+}
 }
 
--- LionStats/static/js/dropdownSummary.js
+++ LionStats/static/js/dropdownSummary.js
@@ -29,40 +29,7 @@ for (var i = 0; i < teamData.data.length; i++)
 }
 }
 
-function dropdownPosition()
-{
-var teamData;
-$.ajax({
-    async: false,
-    url: 'http://localhost:8000/api/dropdown/team',
-    success: function(data)
-    {
-        teamData = data;
-    }
-});
-let dropdown = document.getElementById('dropdownPosition')
-let option;
-while(dropdown.firstChild)
-{
-    dropdown.removeChild(dropdown.firstChild);
-}
 
-for (var i = 0; i < teamData.data.players.length; i++)
-{
-    option = document.createElement("option");
-    option.text = teamData.data.players[i].role;
-    dropdown.appendChild(option);
-}
-var usedNames = {};
-$("select[name='summaryPosition'] > option").each(function () {
-    if(usedNames[this.text]) {
-        $(this).remove();
-    } else {
-        usedNames[this.text] = this.value;
-    }
-});
-
-}
 
 window.onload = function()
 {
